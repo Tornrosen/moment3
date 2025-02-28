efffd6dcc18ed7ebd9676977f350d944b651ee48
Tornrosen <elec11anna@hotmail.com>
2025-02-28 11:28:39 +0100

    ordna bakgrund sass, utseende diagram mm

--- src/js/diagram.js
+++ src/js/diagram.js
@@ -1,8 +1,8 @@
 "use strict";
 
-let diagramData =[];
-let courses =[];
-let programmes=[];
+let diagramData = [];
+let courses = [];
+let programmes = [];
 
 window.onload = () => {
     loadDiagramData();
@@ -13,13 +13,13 @@ window.onload = () => {
 async function loadDiagramData() {
     try {
         const response = await fetch("https://studenter.miun.se/~mallar/dt211g/");
-        if (!response.ok){
+        if (!response.ok) {
             throw new error("Anslutning fungerar ej.")
         }
-        diagramData=await response.json();
+        diagramData = await response.json();
         displayCourses(diagramData);
         displayProgrammes(diagramData);
-    } 
+    }
     catch (error) {
         console.error(error);
     }
@@ -30,51 +30,57 @@ async function loadDiagramData() {
  * @param diagramData
  */
 function displayCourses() {
-    const courses= diagramData.filter(courses=>courses.type=="Kurs");
+    const courses = diagramData.filter(courses => courses.type == "Kurs");
     courses.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
-    let topCourses = courses.slice(0,6);
+    let topCourses = courses.slice(0, 6);
     const barDiagram = document.getElementById('bar').getContext('2d');
-new Chart(barDiagram, {
-    type: 'bar',
-    data: {
-      labels: topCourses.map(course => course.name),
-      datasets: [{
-        label: 'Antal sökande',
-        data: topCourses.map(course => course.applicantsTotal),
-        borderWidth: 10
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+    Chart.defaults.color ='#000000';
+    new Chart(barDiagram, {
+        type: 'bar',
+        data: {
+            labels: topCourses.map(course => course.name),
+            datasets: [{
+                label: 'Antal sökande',
+                data: topCourses.map(course => course.applicantsTotal),
+                borderWidth: 2,
+                backgroundColor:'#000000',
+                borderColor: '#FFFFFF',
+                
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
         }
-      }
-    }
-  });
+    });
 }
 /**
  * funktion som filtrerar, sorterar data, kortar ner array och skriver ut data för program
  * @param diagramData
  */
-    function displayProgrammes() {
-        const programmes= diagramData.filter(programmes=>programmes.type=="Program");
+function displayProgrammes() {
+    const programmes = diagramData.filter(programmes => programmes.type == "Program");
     programmes.sort((a, b) => b.applicantsTotal - a.applicantsTotal);
-    let topProgrammes = programmes.slice(0,5);
+    let topProgrammes = programmes.slice(0, 5);
     const pieDiagram = document.getElementById('pie');
-
-  new Chart(pieDiagram, {
-    type:'pie',
-    data: {
-        labels: topProgrammes.map(programme => programme.name),
-        datasets: [{
-          label: 'Antal sökande',
-          data: topProgrammes.map(programme => programme.applicantsTotal),
-          borderWidth: 1
-        }]  }});
+    new Chart(pieDiagram, {
+        type: 'pie',
+        data: {
+            labels: topProgrammes.map(programme => programme.name),
+            datasets: [{
+                label: 'Antal sökande',
+                data: topProgrammes.map(programme => programme.applicantsTotal),
+                borderWidth: 2,
+                backgroundColor: ['#000000', '#4B164C', '#F5F5F5', '#DD88CF', '#F8E7F6'],
+                borderColor: '#FFFFFF',
+            }]
+        }
+    });
 }
 
 
-  
 
-  
+
